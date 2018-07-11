c03338e73d5b9c7568a91871d323ee5682945884
Wahyu <wahyu@Wahyus-MacBook-Pro.local>
2018-07-12 04:19:03 +0800

    fourth commit array

---
+++ array/array.js
@@ -0,0 +1,20 @@
+//Melakukan looping data array
+
+function dataHandling(n) {
+  
+  for(var i = 0; i < n.length; i++) {
+    console.log("Nomor ID: " + n[i][0]); 
+    console.log("Nama Lengkap: " + n[i][1]);
+    console.log("TTL: " + n[i][2], n[i][3]);
+    console.log("Hobi: " + n[i][4]);
+  }
+}
+
+console.log(dataHandling([
+  ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+  ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+  ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+  ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"], 
+  ["0005", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]));
+
