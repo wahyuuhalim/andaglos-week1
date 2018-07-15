44e9168a357f34c53b8384060532f4044127dd34
Wahyu <wahyu@Wahyus-MacBook-Pro.local>
2018-07-15 14:34:49 +0800

    revisi komentar

--- function/function.js
+++ function/function.js
@@ -5,7 +5,7 @@ function shoutOut() {
   return halo;
 }
 
-console.log(shoutOut()) // Menampilkan "Halo Function!" di console
+console.log(shoutOut()) 
 // Tugas 2
 
 function calculateMultiply(num1,num2) {
@@ -17,7 +17,7 @@ var num1 = 5;
 var num2 = 6;
 
 var hasilPerkalian = calculateMultiply(num1,num2);
-console.log(hasilPerkalian); // Menampilkan angka 30
+console.log(hasilPerkalian); 
 
 // Tugas 3
 
@@ -32,5 +32,5 @@ var address = "Jln. Malioboro, Yogjakarta";
 var hobby = "gaming";
 
 var fullSentence = processSentence(name,age,address,hobby);
-console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan s
+console.log(fullSentence); 
  
--- logic4/logic4a.js
+++ logic4/logic4a.js
@@ -1,8 +1,5 @@
 // Logic Challenge - Target Terdekat
 
-// Problem
-// Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berupa array yang terdiri dari karakter. Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang terdekat. Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0.
-// Code
 function targetTerdekat(arr) {
  // you can only write your code here!
 
